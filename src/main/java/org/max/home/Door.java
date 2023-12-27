b22473c196a72a83a02b9d6ffa74a3585c7052b5
Margarita <margoshka_d@mail.ru>
2023-12-27 20:40:48 +0300

    HW6

---
+++ src/main/java/org/max/home/Door.java
@@ -0,0 +1,26 @@
+package org.max.home;
+
+/**
+ * Класс для описание двери
+ */
+public class Door {
+    private boolean isPrize;
+
+    public Door(boolean isPrize) {
+        this.isPrize = isPrize;
+    }
+
+    public boolean isPrize() {
+        return isPrize;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Door{" +
+                "isPrize=" + isPrize +
+                '}';
+    }
+
+
+}
